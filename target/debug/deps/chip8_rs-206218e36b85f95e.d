c923fc07ecd8474340c7e57123b1e1639a87750c
Nikhil Thomas <nikhil@nthomas.org>
2023-04-19 20:34:18 -0400

    remove target

--- target/debug/deps/chip8_rs-206218e36b85f95e.d
+++
@@ -1,5 +0,0 @@
-/Users/nikhilthomas/Development/chip8-rs/target/debug/deps/chip8_rs-206218e36b85f95e.rmeta: src/main.rs
-
-/Users/nikhilthomas/Development/chip8-rs/target/debug/deps/chip8_rs-206218e36b85f95e.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/chip8_rs-f33f6a65306519a6.d
+++
@@ -1,5 +0,0 @@
-/Users/nikhilthomas/Development/chip8-rs/target/debug/deps/chip8_rs-f33f6a65306519a6.rmeta: src/main.rs
-
-/Users/nikhilthomas/Development/chip8-rs/target/debug/deps/chip8_rs-f33f6a65306519a6.d: src/main.rs
-
-src/main.rs:
